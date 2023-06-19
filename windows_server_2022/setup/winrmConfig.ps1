e5ba56d6f58de927bb57feb1bba6e1f23563eb25
quentinnoilou <135701614+quentinnoilou@users.noreply.github.com>
2023-06-19 11:18:33 +0200

    Update winrmConfig.ps1

--- windows_server_2022/setup/winrmConfig.ps1
+++ windows_server_2022/setup/winrmConfig.ps1
@@ -1,13 +1,16 @@
-rem basic config for winrm
-cmd.exe /c winrm quickconfig -q
+winrm quickconfig -q
+winrm quickconfig -transport:http
+winrm set winrm/config '@{MaxTimeoutms="7200000"}'
+winrm set winrm/config/winrs '@{MaxMemoryPerShellMB="0"}'
+winrm set winrm/config/winrs '@{MaxProcessesPerShell="0"}'
+winrm set winrm/config/winrs '@{MaxShellsPerUser="0"}'
+winrm set winrm/config/service '@{AllowUnencrypted="true"}'
+winrm set winrm/config/service/auth '@{Basic="true"}'
+winrm set winrm/config/client/auth '@{Basic="true"}'
+winrm set winrm/config/listener?Address=*+Transport=HTTP '@{Port="5985"} '
 
-rem allow unencrypted traffic, and configure auth to use basic username/password auth
-cmd.exe /c winrm set winrm/config/service @{AllowUnencrypted="true"}
-cmd.exe /c winrm set winrm/config/service/auth @{Basic="true"}
-
-rem update firewall rules to open the right port and to allow remote administration
-cmd.exe /c netsh advfirewall firewall set rule group="remote administration" new enable=yes
-
-rem restart winrm
-cmd.exe /c net stop winrm
-cmd.exe /c net start winrm
+netsh advfirewall firewall set rule group="remote administration" new enable=yes
+netsh firewall add portopening TCP 5985 "Port 5985"
+net stop winrm
+sc.exe config winrm start= auto
+net start winrm
